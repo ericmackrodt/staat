63dacb78f62f8ddf389ffbe1010906900ccb2ecf
Eric Mackrodt <ericmackrodt@gmail.com>
2019-01-02 18:48:09 +1100

    Fix return type for scoped-transform

--- packages/core/src/scoped-transformer.ts
+++ packages/core/src/scoped-transformer.ts
@@ -57,14 +57,17 @@ export function scopedTransformer<
   TScope
 >(scope: string) {
   return function<TArgs extends any[]>(
-    definition: (currentScope: TScope, ...args: TArgs) => TScope
+    definition: (
+      currentScope: TScope,
+      ...args: TArgs
+    ) => TScope | Promise<TScope>
   ) {
     return function(currentState: TState, ...args: TArgs) {
       const s = getScope<TState, TScope>(currentState, scope);
       const result = definition(s, ...args);
       if (isPromise(result)) {
-        return result.then(scopeResult =>
-          setScope({ ...currentState }, scopeResult, scope)
+        return result.then(promiseResult =>
+          setScope({ ...currentState }, promiseResult, scope)
         );
       }
       return setScope({ ...currentState }, result, scope);
