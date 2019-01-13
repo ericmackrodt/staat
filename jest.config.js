581cdd705fd6b41297ffbd2bd13b09ee82d2fa79
Eric Mackrodt <ericmackrodt@gmail.com>
2019-01-13 14:03:19 +1100

    Add staat tests

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,16 @@
 module.exports = {
-  "collectCoverageFrom": [
-    "packages/**/*.(ts|tsx)",
-    "!packages/**/*.d.(ts|tsx)",
-    "!<rootDir>/node_modules/",
-    "!<rootDir>/**/build/**/*.*",
-    "!<rootDir>/packages/example/**/*.*",
+  collectCoverageFrom: [
+    'packages/**/*.(ts|tsx)',
+    '!packages/**/*.d.(ts|tsx)',
+    '!packages/**/index.(ts|tsx)',
+    '!<rootDir>/node_modules/',
+    '!<rootDir>/**/build/**/*.*',
+    '!<rootDir>/packages/example/**/*.*',
   ],
-  "roots": [
-    "<rootDir>/packages"
-  ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
+  roots: ['<rootDir>/packages'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  "testRegex": "(/__tests__/.*\.test)\.tsx?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-}
+  testRegex: '(/__tests__/.*.test).tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
